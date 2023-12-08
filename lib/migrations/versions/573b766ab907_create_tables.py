c6e68a1794fa668cc3e0e0e6d71f9d41647d0d18
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-08 23:21:00 +0300

    One-to-Many Relationships

---
+++ lib/migrations/versions/573b766ab907_create_tables.py
@@ -0,0 +1,24 @@
+"""Create tables
+
+Revision ID: 573b766ab907
+Revises: faebd12f9523
+Create Date: 2023-12-08 23:04:19.835886
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '573b766ab907'
+down_revision = 'faebd12f9523'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
